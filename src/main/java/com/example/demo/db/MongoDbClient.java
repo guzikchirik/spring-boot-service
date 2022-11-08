e5783dd48ee67441280f89440973116be95a27a0
Anton Chyrko <anton_chyrko@epam.com>
2022-11-08 10:42:28 +0100

    update MongoDbClient

--- src/main/java/com/example/demo/db/MongoDbClient.java
+++ src/main/java/com/example/demo/db/MongoDbClient.java
@@ -11,41 +11,56 @@ import com.example.demo.models.ContactInfo;
 import com.example.demo.models.PersonalInfo;
 import com.example.demo.models.SecurityInfo;
 import com.example.demo.models.UserEntity;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.javafaker.Address;
 import com.github.javafaker.Faker;
 import com.github.javafaker.Name;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.*;
 
 public class MongoDbClient {
 
     public static void main(String[] args) {
-
-//        try (MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017")) {
-//            List<Document> databases = mongoClient.listDatabases().into(new ArrayList<>());
-//            System.out.println("databases amount is " + databases.size());
-//            databases.forEach(db -> System.out.println(db.toJson()));
-//        }
         try (MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017")) {
-            UserEntity userEntity = getUserEntity();
-            Document userEntityDocument = createUserEntityDocument(userEntity);
-            createRecord(mongoClient, userEntityDocument);
+//            createUser(mongoClient);
+//            createUser(mongoClient);
+//            createUser(mongoClient);
+
+            MongoDatabase users = mongoClient.getDatabase("Users");
+            MongoCollection<Document> coll = users.getCollection("Records");
+            System.out.println("countDocuments = " + coll.countDocuments());
+            coll.find().forEach(document -> System.out.println(document.toJson()));
         }
+    }
+
+    public static void createUser(MongoClient mongoClient) {
+        UserEntity userEntity = getUserEntity();
+        Document userEntityDocument = createUserEntityDocument(userEntity);
+        createRecord(mongoClient, userEntityDocument);
     }
 
     public static void createRecord(final MongoClient client, final Document document) {
         MongoDatabase db = client.getDatabase("Users");
 
-        db.getCollection("UsersInfo")
+        db.getCollection("Records")
           .insertOne(document);
     }
 
+//    public Document createUserEntityDocument(final UserEntity userEntity) {
+//        return new Document()
+//                .append("PersonalInfo", userEntity.getPersonalInfo())
+//                .append("SecurityInfo", userEntity.getSecurityInfo())
+//                .append("ContactInfo", userEntity.getContactInfo());
+//    }
+
     public static Document createUserEntityDocument(final UserEntity userEntity) {
-        return new Document()
-                .append("PersonalInfo", userEntity.getPersonalInfo())
-                .append("SecurityInfo", userEntity.getSecurityInfo())
-                .append("ContactInfo", userEntity.getContactInfo());
+        ObjectMapper objectMapper = new ObjectMapper(); //Advisable to create and consume as bean
+        try {
+            return Document.parse(objectMapper.writeValueAsString(userEntity));
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return Document.parse("{}");
     }
 
     private static UserEntity getUserEntity() {
@@ -54,23 +69,29 @@ public class MongoDbClient {
         PersonalInfo personalInfo = PersonalInfo.builder()
                                                 .firstName(name.firstName())
                                                 .lastName(name.lastName())
-                                                .dateOfBirth(faker.date().birthday().toString())
+                                                .dateOfBirth(faker.date()
+                                                                  .birthday()
+                                                                  .toString())
                                                 .gender("male")
                                                 .build();
         SecurityInfo securityInfo = SecurityInfo.builder()
-                                                .email(faker.internet().emailAddress())
-                                                .password(faker.internet().password())
+                                                .email(faker.internet()
+                                                            .emailAddress())
+                                                .password(faker.internet()
+                                                               .password())
                                                 .securityQuestion(SecurityQuestions.FAVORITE_SPORT)
-                                                .securityQuestionAnswer(faker.esports().game())
+                                                .securityQuestionAnswer(faker.esports()
+                                                                             .game())
                                                 .build();
         Address address = faker.address();
         ContactInfo contactInfo = ContactInfo.builder()
-                                       .address(address.fullAddress())
-                                       .city(address.city())
-                                       .state(address.state())
-                                       .zipCode(address.zipCode())
-                                       .phoneNumber(faker.phoneNumber().phoneNumber())
-                                       .build();
+                                             .address(address.fullAddress())
+                                             .city(address.city())
+                                             .state(address.state())
+                                             .zipCode(address.zipCode())
+                                             .phoneNumber(faker.phoneNumber()
+                                                               .phoneNumber())
+                                             .build();
 
         return UserEntity.builder()
                          .personalInfo(personalInfo)
